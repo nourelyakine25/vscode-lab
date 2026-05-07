2544a3561d15151187b5259b67f3a537677bf3d6
nourelyakine25 <messallemyakine@gmail.com>
2026-05-07 21:43:27 +0100

    Refactor variable and method names

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,11 +1,12 @@
 public class Calculator {
-    public double calc(double a, double b) {
-        double x = a + b;
-        double y = a * b;
-        return x / y;
+
+    public double calculateSumProductRatio(double num1, double num2) {
+        double sum = num1 + num2;
+        double product = num1 * num2;
+        return sum / product;
     }
 
-    public void prtRes(double res) {
-        System.out.println("Result: " + res);
+    public void printResult(double result) {
+        System.out.println("Result: " + result);
     }
 }
