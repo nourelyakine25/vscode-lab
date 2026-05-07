e45af974a230a17936093ce6da48a06ae7ba6ba1
nourelyakine25 <messallemyakine@gmail.com>
2026-05-07 22:06:37 +0100

    Replace conditional logic with polymorphism

--- src/main/java/com/example/Employee.java
+++ src/main/java/com/example/Employee.java
@@ -1,14 +1,4 @@
-public class Employee {
+abstract class Employee {
 
-    public double calculateBonus(String type) {
-
-        if (type.equals("Manager")) {
-            return 5000;
-
-        } else if (type.equals("Developer")) {
-            return 3000;
-        }
-
-        return 1000;
-    }
+    abstract double calculateBonus();
 }
