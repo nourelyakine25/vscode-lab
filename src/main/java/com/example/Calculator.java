5b0556ab4e13fae95974cace4cf87af01f69d601
nourelyakine25 <messallemyakine@gmail.com>
2026-05-07 21:41:33 +0100

    Add initial calculator implementation

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public double calc(double a, double b) {
+        double x = a + b;
+        double y = a * b;
+        return x / y;
+    }
+
+    public void prtRes(double res) {
+        System.out.println("Result: " + res);
+    }
+}
